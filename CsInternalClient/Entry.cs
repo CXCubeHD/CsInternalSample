5678c91076f2d207fd17dd9b7d9294d8e483cca8
CXCubeHD <cxcubehd@gmail.com>
2022-09-04 19:04:10 +0200

    Updated Entry.cs

--- CsInternalClient/Entry.cs
+++ CsInternalClient/Entry.cs
@@ -1,3 +1,16 @@
-﻿namespace CsInternalClient;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-public class Class1 { }
+namespace Chrones.IO.Samples.CsInternalClient;
+
+/// <summary>
+/// Represents the entry of this client. Contains the loader function in it.
+/// </summary>
+public static class Entry
+{
+    [UnmanagedCallersOnly(EntryPoint = "_Load_CsInternalClient", CallConvs = new[] { typeof(CallConvStdcall) })]
+    internal static void Load()
+    {
+        
+    }
+}
